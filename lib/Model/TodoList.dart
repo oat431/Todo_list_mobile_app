5d913a052804d2f79524c62f4ab905f682441ec1
Oralita <oat431@gmail.com>
2021-05-17 18:19:28 +0700

    [🥓 modified] : new model for the project

---
+++ lib/Model/TodoList.dart
@@ -0,0 +1,15 @@
+class TodoList {
+  int todo_no;
+  String owner;
+  String description;
+  DateTime date;
+  String title;
+
+  TodoList({
+    @required this.todo_no,
+    @required this.owner,
+    this.description,
+    @required this.date,
+    @required this.title,
+  });
+}
---
+++ lib/Model/TodoTask.dart
@@ -0,0 +1,15 @@
+class TodoTask {
+  int task_no;
+  int todo_no;
+  String task;
+  String description;
+  int status;
+
+  TodoTask({
+    @required this.task_no,
+    @required this.todo_no,
+    @required this.task,
+    this.description,
+    @required this.status,
+  });
+}
