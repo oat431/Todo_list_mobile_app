959c810e5345894725631f8dab3b32beb575a450
Oralita <oat431@gmail.com>
2021-05-20 15:22:32 +0700

    [ 🥓 modified ] : the home page app properties

--- lib/Component/HomePageAppbar.dart
+++ lib/Component/HomePageAppbar.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/Widget/TodoAppbar.dart';
 
-class HomeAppbar extends StatelessWidget {
+class HomeAppbar extends StatelessWidget implements PreferredSizeWidget {
+  String title;
+  HomeAppbar({this.title});
+
+  @override
+  Size get preferredSize => Size.fromHeight(50);
+
   @override
   Widget build(BuildContext context) {
-    return TodoAppbar(title:'TodoList');
+    return TodoAppbar(title: title);
   }
 }
