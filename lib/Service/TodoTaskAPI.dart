6837d568e2162f9654a754286cdf63e9a7045557
Oralita <oat431@gmail.com>
2021-05-20 15:24:51 +0700

    [ 🥩 add ] : all necessary api for this application

---
+++ lib/Service/TodoTaskAPI.dart
@@ -0,0 +1,6 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:todo_list/Model/TodoTask.dart';
+
+Future<TodoTask> getAllTask(int id) async {}
---
+++ lib/Service/TosoListAPI.dart
@@ -0,0 +1,6 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:todo_list/model/TodoList.dart';
+
+Future<TodoList> getAllList() async {}
